bf59cb1c6ef4fc48e5a8503a740b833376cc3e0e
nick <wrl.snk@gmail.com>
2024-06-22 05:11:14 +0300

    one more snippet

---
+++ src/swapTextStyleGroups.ts
@@ -0,0 +1,31 @@
+const mapping = {
+	from: "Medium",
+	to: "Large"
+}
+
+let count = 0
+let froms = Object.keys(mapping)
+
+export const swapTextStyleGroups = async () => {
+	const textStyles = await figma.getLocalTextStylesAsync()
+	swapTextStyles(figma.currentPage.selection, textStyles)
+	figma.notify(`Swapped ${count} properties`)
+}
+
+async function swapTextStyles(nodes, textStyles) {
+	for (const node of nodes) {
+		const textStyleId = node.textStyleId
+		if (textStyleId && textStyleId !== figma.mixed) {
+			const styleName = (await figma.getStyleByIdAsync(textStyleId)).name
+			let [prefix, ...name] = styleName.split('/')
+			name = name.join('/')
+			if (prefix.toLowerCase() === mapping.from.toLowerCase()) {
+				node.textStyleId = textStyles.find(style => style.name === (mapping.from + name)).id
+				count++
+			}
+		}
+		if (node.children.length > 0) {
+			await swapTextStyles(node.children, textStyles)
+		}
+	}
+}
